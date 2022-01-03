40e6c64247428f0cd9e7660854b33ae84c3af2d4
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-01-03 17:19:50 +0100

    Losing mind

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+// import React from "react"; dont need this anymore. was required for every component
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Header;
